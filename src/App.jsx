0e1f115ae0f23c919c0f4bb65a74bff8fdd77e2d
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-06-25 22:07:39 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline text-sky-400">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
