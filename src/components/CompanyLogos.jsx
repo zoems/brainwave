3dc79261f07a5bbc6e073ca8a702381209dac1d0
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-07-03 14:33:56 +0200

    feat(hero): add partners marquee

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,21 +1,53 @@
 import { companyLogos } from "../constants";
+import "../index.css"; // Import the CSS for styling
 
 const CompanyLogos = ({ className }) => {
+  const logosperrow = Math.ceil(companyLogos.length / 3);
+  const rowlogos1 = companyLogos.slice(0, logosperrow);
+  const rowlogos2 = companyLogos.slice(logosperrow, 2 * logosperrow);
+  const rowlogos3 = companyLogos.slice(2 * logosperrow);
+
   return (
-    <div className={className}>
+    <div className={`${className} marquee`}>
       <h2 className="mb-6 body-1 tagline text-xl text-center text-n-1/50">
         Trusted By
       </h2>
-      <ul className="flex">
-        {companyLogos.map((logo, index) => (
-          <li
-            className="flex items-center justify-center flex-1 h-[8.5rem]"
-            key={index}
-          >
-            <img src={logo} width={134} height={28} alt={logo} />
-          </li>
-        ))}
-      </ul>
+      <div className="marquee-content 1">
+        {[...rowlogos1, ...rowlogos1].map(
+          (
+            logo,
+            index // Duplicate logos for a seamless loop
+          ) => (
+            <div className="marquee-logo " key={`row1-${index}`}>
+              <img src={logo} width={134} height={28} alt={`Logo ${index}`} />
+            </div>
+          )
+        )}
+      </div>
+      <div className="marquee-pos-content 2">
+        {[...rowlogos2, ...rowlogos2].map(
+          (
+            logo,
+            index // Duplicate logos for a seamless loop
+          ) => (
+            <div className="marquee-logo" key={`row2-${index}`}>
+              <img src={logo} width={134} height={28} alt={`Logo ${index}`} />
+            </div>
+          )
+        )}
+      </div>
+      <div className="marquee-content 3">
+        {[...rowlogos3, ...rowlogos3].map(
+          (
+            logo,
+            index // Duplicate logos for a seamless loop
+          ) => (
+            <div className="marquee-logo" key={`row3-${index}`}>
+              <img src={logo} width={134} height={28} alt={`Logo ${index}`} />
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 };
