61e8ea636d29f7aed454bb172674316e0e2976d8
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-07-05 14:46:51 +0200

    fix: pointer conflicts

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -23,7 +23,7 @@ const Benefits = () => {
               }}
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem]">
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
@@ -36,18 +36,17 @@ const Benefits = () => {
                   <a
                     className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider"
                     href={item.url}
-                    // onClick={onClick}
                   >
                     Explore more
                   </a>
-
                   <Arrow />
                 </div>
               </div>
+
               {item.light && <GradientLight />}
 
               <div
-                className="absolute inset-0.5 bg-n-8"
+                className="absolute inset-0.5"
                 style={{ clipPath: "url(#benefits)" }}
               >
                 <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
