27912373c18ca9d4b20a15c8e8b6b88c63adc8d0
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-07-05 10:43:56 +0200

    fix(hero): partners animation

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -8,25 +8,25 @@ const CompanyLogos = ({ className }) => {
   const rowlogos3 = companyLogos.slice(2 * logosperrow);
 
   return (
-    <div className={`${className} marquee`}>
-      <h2 className="mb-6 body-1 tagline text-xl text-center text-n-1/50">
+    <div className={`${className} marquee `}>
+      <h2 className="mb-6 body-1 tagline text-xl text-center text-n-1/50 ">
         Trusted By Industry Leaders
       </h2>
-      <div className="marquee-content 1">
+      <div className="marquee-content 1 md:w-[calc(290%)] sm:w-[calc(541%)] lg:w-[calc(222%)]">
         {[...rowlogos1, ...rowlogos1].map((logo, index) => (
           <div className="marquee-logo " key={`row1-${index}`}>
             <img src={logo} width={134} height={28} alt={`Logo ${index}`} />
           </div>
         ))}
       </div>
-      <div className="marquee-pos-content 2">
+      <div className="marquee-pos-content 2 md:w-[calc(290%)] sm:w-[calc(541%)] lg:w-[calc(222%)]">
         {[...rowlogos2, ...rowlogos2].map((logo, index) => (
           <div className="marquee-logo" key={`row2-${index}`}>
             <img src={logo} width={134} height={28} alt={`Logo ${index}`} />
           </div>
         ))}
       </div>
-      <div className="marquee-content 3">
+      <div className="marquee-content 3 md:w-[calc(290%)] sm:w-[calc(541%)] lg:w-[calc(222%)]">
         {[...rowlogos3, ...rowlogos3].map((logo, index) => (
           <div className="marquee-logo" key={`row3-${index}`}>
             <img src={logo} width={134} height={28} alt={`Logo ${index}`} />
