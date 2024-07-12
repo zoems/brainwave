4442c3930159717a69e55a199b320e14122fd4b5
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-07-12 20:03:34 +0200

    feat: add about page

---
+++ src/components/design/About.jsx
@@ -0,0 +1,30 @@
+import { gradient } from "../../assets";
+import brackets from "../../assets/svg/Brackets";
+
+export const Gradient = () => {
+  return (
+    <div className="absolute top-[18.25rem] -left-[30.375rem] w-[56.625rem] opacity-60 mix-blend-color-dodge pointer-events-none">
+      <div className="absolute top-1/2 left-1/2 w-[58.85rem] h-[58.85rem] -translate-x-3/4 -translate-y-1/2">
+        <img
+          className="w-full"
+          src={gradient}
+          width={942}
+          height={942}
+          alt="Gradient"
+        />
+      </div>
+    </div>
+  );
+};
+
+export const TagLine = ({ className, children }) => {
+  return (
+    <div className={`tagline flex items-center ${className || ""}`}>
+      {brackets("left")}
+      <div className="mx-3 text-n-3">{children}</div>
+      {brackets("right")}
+    </div>
+  );
+};
+
+export default TagLine;
