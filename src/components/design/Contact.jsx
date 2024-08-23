16b20345d1a09dcf5ae564614a1660343cf9bb93
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-08-23 16:12:29 +0200

    fix():..

--- src/components/design/Contact.jsx
+++ src/components/design/Contact.jsx
@@ -3,7 +3,7 @@ import { Html } from "@react-three/drei";
 const Loader = () => {
   return (
     <Html>
-      <div className="flex justify-center items-center">
+      <div className=" z-100 flex justify-center items-center">
         <div className="w-20 h-20 border-2 border-opacity-20 border-blue-100 border-t-blue-300 rounded-full animate-spin"></div>
       </div>
     </Html>
---
+++ src/components/design/Loader.jsx
@@ -0,0 +1,8 @@
+export const Loader = () => {
+  return (
+    <div className=" z-100 flex justify-center items-center">
+      <div className="w-20 h-20 border-2 border-opacity-20 border-blue-100 border-t-blue-300 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+export default Loader;
