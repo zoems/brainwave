c1d15724bbefb3bd5db01b408ad9611ac7524bea
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-08-30 12:46:19 +0200

    fix(logos): margins and padding

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -6,14 +6,14 @@ const CompanyLogos = ({ className }) => {
     <div
       className={`${className} relative z-10 max-w-[23rem] mx-auto md:max-w-[43rem] lg:max-w-[56.1rem] mt-20 block marquee `}
     >
-      <h2 className="mb-6 body-1 tagline text-xl text-center text-n-1/50 ">
+      <h2 className="mb-10 body-1 tagline text-xl text-center text-n-1/50 ">
         Trusted By Industry Leaders
       </h2>
-      <div className=" p-3 bg-white">
-        <div className="flex flex-wrap justify-center">
+      <div className="p-5 bg-white">
+        <div className="flex pt-5 flex-wrap justify-center">
           {companyLogos.map((logo, index) => (
             <div className="marquee-logo " key={`row1-${index}`}>
-              <img src={logo} width={80} height={28} alt={`Logo ${index}`} />
+              <img src={logo} width={90} height={28} alt={`Logo ${index}`} />
             </div>
           ))}
         </div>
