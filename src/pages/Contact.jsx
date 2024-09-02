c258d11197ccc5071e460505c27b9462594eb0f2
Zoe Mawby Smart <zoemsmart@gmail.com>
2024-09-02 13:19:41 +0200

    fix(canvas): z-index and move to bg

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -141,7 +141,7 @@ const Contact = () => {
               </Button>
             </form>
           </div>
-          <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px] bg-radial-gradient from-[#0E3B8F] to-[#28206C]/0 to-70% pointer-events-none">
+          <div className="absolute -z-1 1 w-full h-full bg-radial-gradient from-[#0E3B8F] to-[#28206C]/0 to-70% pointer-events-none">
             {" "}
             <Canvas
               camera={{
@@ -164,6 +164,7 @@ const Contact = () => {
               </Suspense>
             </Canvas>
           </div>
+          <div></div>
         </div>
       </Section>
       <Footer />
